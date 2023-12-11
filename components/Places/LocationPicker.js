72ae5192ceff53d0c709ab69e9fc713c2903ee59
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-11 11:59:44 +0100

    new folder, first folder corrupted

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,48 @@
+import { StyleSheet, View } from "react-native";
+import { getCurrentPositionAsync } from "@expo-location";
+
+import OutlinedButton from "../ui/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
+function LocationPicker() {
+  async function getLocationHandler() {
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton onPress={getLocationHandler} icon="location">
+          Locate User
+        </OutlinedButton>
+
+        <OutlinedButton onPress={pickOnMapHandler} icon="map">
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
